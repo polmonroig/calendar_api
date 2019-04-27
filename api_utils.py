45bdd10dbb95b220254c144dddfc8f4b213ba6df
Pol Monroig <polmonroig@yahoo.es>
2019-04-27 14:55:40 +0200

    Fix max entries and add start and end date

--- api_utils.py
+++ api_utils.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-import datetime
 import pickle
 import os.path
 from googleapiclient.discovery import build
@@ -9,41 +8,42 @@ from google.auth.transport.requests import Request
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
+
 def connect():
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
-    creds = None
+    credentials = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+            credentials = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server()
+            credentials = flow.run_local_server()
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+            pickle.dump(credentials, token)
 
-    service = build('calendar', 'v3', credentials=creds)
+    service = build('calendar', 'v3', credentials=credentials)
 
     return service
 
 
-def get_list(service, maxResults):
+def get_list(service, start_time, end_time):
 
     # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
     id = 'primary'
     events_result = service.events().list(calendarId=id,
-                                          timeMin=now,maxResults=maxResults,
+                                          timeMin=start_time,
+                                          timeMax=end_time,
                                           singleEvents=True,
                                           orderBy='startTime').execute()
     events = events_result.get('items', [])
--- events_list.py
+++ events_list.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtGui, QtCore, QtWidgets
 from api_utils import connect, get_list
+import datetime
 
 
 class EventsList(QtWidgets.QListWidget):
@@ -10,34 +11,51 @@ class EventsList(QtWidgets.QListWidget):
         QtWidgets.QListWidget.__init__(self, parent=parent)
         self.service = None
         self.color = None
-        self.max_entries = 10
+        self.max_entries = None
+        self.start_time = datetime.datetime.utcnow().isoformat() + 'Z'
+        self.end_time = None
+        self.search_entry = None
 
     def set_max_entries(self, entries):
-        self.max_entries = int(entries)
+        try:
+            self.max_entries = int(entries)
+        except ValueError:
+            self.max_entries = None
 
     def empty_list(self):
         while self.count() > 0:
             self.takeItem(0)
 
+    def set_search_entry(self, s):
+        self.search_entry = s
+
+    def set_end_time(self, time):
+        self.end_time = time.toPyDateTime().isoformat() + 'Z'
+
+    def set_start_time(self, time):
+        self.start_time = time.toPyDateTime().isoformat() + 'Z'
+
     def add_list_elements(self):
-        elements = get_list(self.service, self.max_entries)
+        print("Events: ")
+        elements = get_list(self.service, self.start_time,
+                            self.end_time)
         self.empty_list()
         count = 0
+        print(len(elements))
         for e in elements:
-            if 'colorId' in e:
-                color = e['colorId']
+            if self.color is None:
+                self.addItem(e['summary'])
+                count += 1
+            elif 'colorId' in e and int(e['colorId']) == self.color:
+                self.addItem(e['summary'])
+                count += 1
                 print(e['summary'], e['colorId'])
-                if self.color is None:
-                    self.addItem(e['summary'])
-                    count += 1
-                elif int(color) == self.color:
-                    self.addItem(e['summary'])
-                    count += 1
+            if self.max_entries and count >= self.max_entries:
+                break
         self.send_entries.emit(count)
 
     def connect_api(self):
         self.service = connect()
-
 
     def filter_color(self, color):
         print("Current filter:", color)
